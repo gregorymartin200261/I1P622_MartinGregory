c966780c4b872c68afd8625fb94480aeff4d3801
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-10-04 14:32:35 +0200

    Add files via upload

---
+++ I1P622_MartinGregory/I1P622_MartinGregory/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace I1P622_MartinGregory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ColorBase = 3;
+            int feuxId = 01;
+
+            //new int[] = utils(feux.feuxRouge)
+            Console.WriteLine("Etat des feux:");
+            EtatFeuBase(ColorBase, feuxId);
+            Console.WriteLine("Chéangement d'etat:");
+            EtatFeu(ColorBase, feuxId);
+            Console.WriteLine("Feu clignotant:");
+            ClignoFeu(ColorBase, feuxId);
+            Console.ReadLine();
+            
+        }
+
+        static void EtatFeuBase(int ColorBase, int feuxId)
+        {
+
+            string Color;
+            if (ColorBase==1)
+            {
+                 Color = "vert";
+            }
+            else if (ColorBase == 2)
+            {
+                 Color = "orange";
+            }
+            else
+            {
+                 Color = "rouge";
+            }
+
+            Console.WriteLine("Le feu de signalisation "+feuxId+" est "+ Color);
+        }
+        static void EtatFeu(int ColorBase, int feuxId)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int J = 0; J < 5; J++)
+                {
+                   
+                }
+                if(ColorBase<3)
+                {
+                    ColorBase++;
+                }
+                else
+                { 
+                    ColorBase = 1;
+                        
+                }
+                string Color;
+                if (ColorBase == 1)
+                {
+                    Color = "vert";
+                }
+                else if (ColorBase == 2)
+                {
+                    Color = "orange";
+                }
+                else
+                {
+                    Color = "rouge";
+                }
+                Console.WriteLine("Le feu de signalisation " + feuxId + " est " + Color);
+            }
+        }
+        static void ClignoFeu(int ColorBase, int feuxId)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                
+                
+                if (ColorBase < 3)
+                {
+                    ColorBase++;
+                }
+                else
+                {
+                    ColorBase = 1;
+
+                }
+                
+                string Color;
+                if (ColorBase == 1)
+                {
+                    Color = "vert";
+                }
+                else if (ColorBase == 2)
+                {
+                    Color = "orange";
+                }
+                else
+                {
+                    Color = "rouge";
+                }
+                Console.WriteLine("Le feu de signalisation " + feuxId + " est " + Color);
+            }
+        }
+
+
+    }
+}
---
+++ I1P622_MartinGregory/I1P622_MartinGregory/feux.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace I1P622_MartinGregory
+{
+    class feux
+    {
+        private void feuxRouge()
+        {
+            string _feuxId;
+            int _ColorBase;
+            bool _feuClignotant;
+
+           
+        }
+
+        public void feu()
+        {
+            //string feuxId=this._feuxId;
+        }
+    }
+}
